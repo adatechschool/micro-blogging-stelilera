fdf97b4b2898d776678b52eb5136f1056b011b10
leyanelouiset <louisetleyane@gmail.com>
2025-04-07 12:10:33 +0200

    install server

---
+++ index.js
@@ -0,0 +1,18 @@
+const express =require('express');
+
+const app =express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+
+// Route de test
+app.get('/', (req, res) => {
+  res.json({ message: 'API fonctionne correctement!' });
+});
+
+
+
+app.listen(port, () => {
+  console.log(`Serveur en cours d'exécution sur le port http://localhost:${port}`);
+});
