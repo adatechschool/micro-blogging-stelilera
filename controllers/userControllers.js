61d62dd9dfcba40a6f70bd92d18c8bce848cdbca
AssiarDev <halibaraissa@gmail.com>
2025-04-08 18:07:14 +0200

    [ADD] route register methode resigter et middleware auth mais pas encore fonctionnel

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -13,9 +13,9 @@ export async function handleGetAllUsers(req, res) {
   export async function handleCreateUser (req,res) {
     try{
       const data = req.body;
-      await User.create(data);
+      await User.register(data);
       res.status(200).json(data);
     }catch(error){
     res.status(500).json({ error: 'Erreur lors de la création de utilisateur.' });
     }
-    }
+  }
--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { handleCreateUser, handleGetAllUsers } from '../controllers/userControllers.js';
+//import { authCheck } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/users', handleGetAllUsers);
-router.post('/users', handleCreateUser)
+router.post('/register', handleCreateUser)
 
 export {router as users};
