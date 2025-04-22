dbeae5e8f1cdc3acb5e787df1bc0b55e3c2a58d3
stellab95 <stellab95@hotmail.fr>
2025-04-22 15:50:13 +0200

    add script.js

---
+++ public/script.js
@@ -0,0 +1,34 @@
+// Se connecter 
+const handleSubmit = async (e) => {
+    try{
+        e.preventDefault();
+
+    // récupérer les données de l'utilisateur
+    const mail = e.target.email.value;
+    const password = e.target.password.value;
+console.log(password);
+
+    const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/json'
+        },
+        body: JSON.stringify({ mail, password })
+    });
+
+    if(!response.ok){
+        throw new Error(`Echec tentative de reponse ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log('result :', result)
+
+    window.location.href = '/';
+    } catch (e){
+        console.error('Erreur lors de la soumission :', e.message)
+    }
+};
+
+const form = document.querySelector('.login-form');
+console.log(form);
+document.querySelector('.login-form').addEventListener('submit', handleSubmit);
