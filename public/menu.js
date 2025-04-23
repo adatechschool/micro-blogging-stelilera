875d5a33885debcbd67065449af725689a73a1bc
stellab95 <stellab95@hotmail.fr>
2025-04-23 09:19:17 +0200

    add menu

---
+++ public/menu.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const userIcon = document.querySelector(".user-link");
+    const dropdown = document.querySelector(".user-dropdown");
+  
+    userIcon.addEventListener("click", (e) => {
+      e.preventDefault();
+      dropdown.style.display = dropdown.style.display === "block" ? "none" : "block";
+    });
+  
+    // Optionnel : fermer le menu quand on clique ailleurs
+    document.addEventListener("click", (e) => {
+      if (!e.target.closest(".user-menu-container")) {
+        dropdown.style.display = "none";
+      }
+    });
+  });
+  
